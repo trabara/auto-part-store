9879c52805908c0ee03d6825f1fd60457ca83ab8
obha <obha121@gmail.com>
2026-03-30 02:21:23 +0100

    feat(rbac-plugin): update RBAC module name to rbacV2

--- packages/rbac-plugin/src/modules/rbac/index.ts
+++ packages/rbac-plugin/src/modules/rbac/index.ts
@@ -2,7 +2,7 @@ import { Module } from "@medusajs/framework/utils";
 import createRbacLoader from "./loaders/create-rbac";
 import RbacModuleService from "./services/rbac.service";
 
-export const RBAC_MODULE = "roleBaseAccessControl";
+export const RBAC_MODULE = "rbacV2";
 
 export default Module(RBAC_MODULE, {
   service: RbacModuleService,
